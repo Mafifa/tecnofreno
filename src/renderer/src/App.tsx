74011a5aa790361f9e1934a42e09f8fd76b56459
Mafifa <charlysmafifa@gmail.com>
2025-04-03 14:21:47 -0400

    Agregamos icono

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
               <div className="flex items-center">
-                <h1 className={`text-2xl font-bold ${modoOscuro ? 'text-blue-400' : 'text-blue-600'} tracking-tight`}>TECNOFRENO 1.0.3</h1>
+                <h1 className={`text-2xl font-bold ${modoOscuro ? 'text-blue-400' : 'text-blue-600'} tracking-tight`}>TECNOFRENO</h1>
               </div>
               <div className="flex items-center space-x-1">
                 {tabs.map((tab) => (

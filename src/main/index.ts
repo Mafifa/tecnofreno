45f6ede95b9d1bdbf8445fbaa58afba490e951dd
Mafifa <charlysmafifa@gmail.com>
2025-04-05 13:52:35 -0400

    actualizador terminado

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,9 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import { setupAllControllers } from './controllers'
+import { autoUpdater } from 'electron-updater'
 
 function createWindow(): void {
   // Create the browser window.
@@ -28,24 +29,68 @@ function createWindow(): void {
   })
 
   // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  // Auto-update event listeners
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for updates...')
+  })
+
+  autoUpdater.on('update-available', () => {
+    dialog
+      .showMessageBox(mainWindow, {
+        type: 'info',
+        title: 'Actualización Disponible',
+        message: 'Hay una nueva versión disponible. ¿Deseas descargarla ahora?',
+        buttons: ['Sí', 'No']
+      })
+      .then(({ response }) => {
+        if (response === 0) {
+          autoUpdater.downloadUpdate()
+        }
+      })
+  })
+
+  autoUpdater.on('update-not-available', () => {
+    console.log('No update available.')
+  })
+
+  autoUpdater.on('error', (err) => {
+    dialog.showErrorBox('Error al buscar actualizaciones', err.message)
+  })
+
+  autoUpdater.on('download-progress', (progressObj) => {
+    console.log(`Download speed: ${progressObj.bytesPerSecond}`)
+    console.log(`Downloaded ${progressObj.percent}%`)
+  })
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog
+      .showMessageBox(mainWindow, {
+        type: 'info',
+        title: 'Actualización Descargada',
+        message:
+          'La actualización ha sido descargada. ¿Deseas reiniciar la aplicación para instalarla?',
+        buttons: ['Reiniciar Ahora', 'Más Tarde']
+      })
+      .then(({ response }) => {
+        if (response === 0) {
+          autoUpdater.quitAndInstall()
+        }
+      })
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// This method will be called when Electron has finished initialization
 app.whenReady().then(() => {
-  // Set app user model id for windows
+  // Set app user model id for Windows
   electronApp.setAppUserModelId('com.electron')
 
   // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
@@ -56,16 +101,16 @@ app.whenReady().then(() => {
   setupAllControllers()
   createWindow()
 
+  // Check for updates automatically after the app is ready
+  autoUpdater.checkForUpdates()
+
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // On macOS, re-create a window in the app when the dock icon is clicked
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed, except on macOS
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
